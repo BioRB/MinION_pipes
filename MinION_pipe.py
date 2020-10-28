f1632d1469435177f4297c1de90659fe98602c9c
BioRB <quasibiologo@gmail.com>
2020-10-28 16:46:15 +0100

    fig

--- MinION_pipe.py
+++ MinION_pipe.py
@@ -1,16 +1,19 @@
 import sys
 import subprocess
 from os import system
-
-fast5_path = sys.argv[1]
-flowcell = sys.argv[2]
-kit = sys.argv[3]
-threads = sys.argv[4]
-num = sys.argv[5]
-barcode = sys.argv[6]
-medaka_m = sys.argv[7]
-subprocess.call(["guppy_basecaller", "--input_path", fast5_path, "--save_path", "./guppy_out", "--flowcell", flowcell,
-                 "--kit", kit, "--cpu_threads_per_caller", threads, "--num_callers", num, "--barcode_kits", barcode,
+import argparse
+parser = argparse.ArgumentParser(description='Assemble genome from MinION sequencing data')
+parser.add_argument("fast5_path", "-f5", help="path to fast5 files", type=str)
+parser.add_argument("--flowcell", "-fc", help="the code of flowcell used (ex. FLO-MIN106)", type=str)
+parser.add_argument("--kit", "-k", help="the MinION kit used (ex. SQK-LSK109)", type=str)
+parser.add_argument("--threads", "-t", help="threads to be used (ex. 16)", type=int)
+parser.add_argument("--num", "-n", help="number of callers to use (ex. 8)", type=int)
+parser.add_argument("--barcode", "-bc", help="the code of the barcodes kit used (ex. EXP-PBC001 )", type=int)
+parser.add_argument("--medaka_m", "-n", help="define a model based on the basecaller (ex. r941_min_high_g303) - "
+                                             "see medaka tool for more details -", type=int)
+args = parser.parse_args()
+subprocess.call(["guppy_basecaller", "--input_path", args.fast5_path, "--save_path", "./guppy_out", "--flowcell", args.flowcell,
+                 "--kit", args.kit, "--cpu_threads_per_caller", args.threads, "--num_callers", args.num, "--barcode_kits", args.barcode,
                  "--trim_barcodes"])
 subprocess.call(["find", "./guppy_out/", "-name", "*.fastq", "-exec", "cat", "{}", "+", ">", "run_all.fastq"])
 system('paste - - - - < run_all.fastq | cut -f 1,2 | sed "s/^@/>/" | tr "\t" "\n" | sed "s/ //g" > run_all.fasta')
@@ -18,6 +21,6 @@ system('NanoFilt sequences.fastq --maxlength 8000 | gzip > NF_trimmed-reads.fast
        '--keep_percent 10 NF_trimmed-reads.fastq.gz | gzip > FL_output.fastq.gz')
 system('canu -p run -d canu -nanopore-corrected FL_output.fastq.gz -genomesize=8k –assemble')
 subprocess.call(['medaka_consensus', '-i', 'FL_output.fastq.gz', '-d', './canu/*.contigs.fasta', '-o', 'medaka-out',
-                 '-t', threads, '-m', medaka_m])
+                 '-t', args.threads, '-m', args.medaka_m])
 print("you can find the final consensus sequence inside the folder medaka-out. The name of the file is consensus.fasta"
       "Bye!")
